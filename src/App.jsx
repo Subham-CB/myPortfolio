913992f000bd2d8b1cc3bbbaabdb7e6da59fced8
Subham <de.subham1@gmail.com>
2026-03-19 14:16:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from "./sections/Hero.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <div>
+                <Hero />
+            </div>
+        </main>
+
+    )
+}
+export default App
